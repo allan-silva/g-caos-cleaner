5672e44bb28eddc40d87968f622cbf744d1bbb6a
Allan Silva <alan@allansilva.com.br>
2022-12-25 20:21:25 -0300

    Exploratory tests

---
+++ g-caos-cleaner/src/test/scala/exploratory/AuthFlowExploratory.scala
@@ -0,0 +1,149 @@
+package exploratory
+
+import com.google.api.client.auth.oauth2.{BearerToken, Credential}
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.gson.GsonFactory
+import com.google.api.services.gmail.Gmail
+import munit.FunSuite
+import org.apache.hc.client5.http.impl.classic.HttpClients
+import org.apache.hc.client5.http.impl.io.LenientHttpResponseParser
+import org.apache.hc.core5.http.ProtocolVersion
+import org.apache.hc.core5.http.config.Http1Config
+import org.apache.hc.core5.http.impl.io.{DefaultClassicHttpResponseFactory, DefaultHttpRequestParser, DefaultHttpResponseWriter, SessionInputBufferImpl, SessionOutputBufferImpl}
+import org.apache.hc.core5.http.message.{BasicClassicHttpResponse, BasicLineParser}
+import org.apache.http.client.utils.URLEncodedUtils
+import org.apache.http.impl.io.HttpTransportMetricsImpl
+
+import java.net.ServerSocket
+import java.io.{BufferedReader, ByteArrayInputStream, ByteArrayOutputStream, InputStreamReader}
+import java.net.Socket
+import java.nio.CharBuffer
+import java.nio.charset.StandardCharsets
+import scala.language.postfixOps
+import scala.util.control.Breaks.{break, breakable}
+import scala.util.Using
+import scala.collection.mutable
+import sttp.client3.*
+
+import scala.sys.process.*
+import sttp.client3.upicklejson.*
+import upickle.default.*
+
+
+class AuthFlowExploratory extends FunSuite {
+
+  val ContentLengthHeader = "Content-Length: "
+
+  test("Setup Standalone HttpServer") {
+    println(wait_for_response())
+  }
+
+  test("Apache response write") {
+    val basicResponse = BasicClassicHttpResponse(200, "OK")
+    val output = ByteArrayOutputStream()
+    val outputSession = SessionOutputBufferImpl(512, StandardCharsets.UTF_8.newEncoder())
+    val responseWriter = DefaultHttpResponseWriter()
+    responseWriter.write(basicResponse, outputSession, output)
+    outputSession.flush(output)
+
+    assert(output.toString(StandardCharsets.UTF_8) == "HTTP/1.1 200 OK\r\n\r\n")
+
+  }
+
+  test("Apache read request") {
+    val server = ServerSocket(9999)
+    val clientSocket = server.accept
+
+    val requestParser = DefaultHttpRequestParser()
+    val sessionInputBuffer = SessionInputBufferImpl(512)
+    val parsedRequest = requestParser.parse(sessionInputBuffer, clientSocket.getInputStream)
+    val queryParams = URLEncodedUtils.parse(parsedRequest.getUri, StandardCharsets.UTF_8)
+    val queryMap = mutable.Map.empty[String, String]
+
+    queryParams.forEach(queryParam => queryMap += (queryParam.getName -> queryParam.getValue))
+
+    println(queryMap("test"))
+
+    assert(queryMap contains "test")
+  }
+
+  test("Auth flow - Open Browser") {
+    "xdg-open http://localhost:5000".!
+    val code_response = wait_for_response()
+    println(code_response)
+  }
+
+  test("Auth flow - Request access token") {
+    val client = SimpleHttpClient()
+    val request = basicRequest.post(uri"http://localhost:5000/access-token")
+      .body(AuthAccessTokenRequest("4/0AWgavdcVShLIgShUCn6uIjUWuUBgLv9lBvsK3CxxwAwaspG4amANyVZbqRzej_qPgedUCA"))
+    val response = client.send(request)
+    response.body match {
+      case Left(e) => println(s"Got response exception:\n$e")
+      case Right(r) => println(r)
+    }
+  }
+
+  test("Call google api") {
+    val transport = GoogleNetHttpTransport.newTrustedTransport()
+    val credential = Credential.Builder(BearerToken.authorizationHeaderAccessMethod())
+      .setTransport(transport)
+      .setJsonFactory(GsonFactory.getDefaultInstance)
+      .build()
+    credential.setAccessToken("ya29.a0AX9GBdXqoF-NpnF10CqVXqW3EQLsP_yl_m8kUb6MV6lc9ox5xrwseVkNZth32gAT2UK0qE_I6rLdstIxC9upR3F0xp0zWZcZjoqWC7KX0otlM3hv0yqrqIUoWg0M-qhGUKxIociy4elhtn5DB2sfvhzZ93hlaCgYKAdoSARMSFQHUCsbCMmFjs5FqK7ykvRk94KR6zw0163")
+    val gmail = Gmail.Builder(transport, GsonFactory.getDefaultInstance, credential).setApplicationName("g-caos-cleaner").build()
+    val labelsResponse = gmail.users.labels.list("me").execute
+    labelsResponse.getLabels.stream().map(label => label.getName).forEach(println)
+  }
+
+  case class AuthAccessTokenRequest(authorization_code: String)
+  implicit val myRequestRW: ReadWriter[AuthAccessTokenRequest] = macroRW[AuthAccessTokenRequest]
+
+  def wait_for_response(): String =
+    val server = ServerSocket(9999)
+    val clientSocket = server.accept
+    val uri = read_response(clientSocket)
+    write_response(clientSocket)
+    clientSocket.close()
+    server.close()
+    uri
+
+  def write_response(clientSocket: Socket): Unit =
+    val outputSession = SessionOutputBufferImpl(512, StandardCharsets.UTF_8.newEncoder())
+    val responseWriter = DefaultHttpResponseWriter()
+    responseWriter.write(httpResponse(), outputSession, clientSocket.getOutputStream)
+    outputSession.flush(clientSocket.getOutputStream)
+
+  def httpResponse(): BasicClassicHttpResponse = BasicClassicHttpResponse(200, "OK")
+
+  def get_body(clientBuffer: BufferedReader): String = get_body(clientBuffer, 0, false)
+
+  def read_response(clientSocket: Socket): String =
+    val requestParser = DefaultHttpRequestParser ()
+    val sessionInputBuffer = SessionInputBufferImpl (512)
+    val parsedRequest = requestParser.parse (sessionInputBuffer, clientSocket.getInputStream)
+    parsedRequest.getRequestUri
+
+  def get_body(clientBuffer: BufferedReader, contentLength: Int, startBody: Boolean): String =
+    if contentLength > 0 && startBody then {
+      val buffer = CharBuffer allocate contentLength
+      clientBuffer.read(buffer)
+      return String(buffer.array)
+    }
+
+    val line = clientBuffer.readLine()
+    println(line)
+
+    if line.isBlank then {
+      if contentLength == 0 then {
+        return ""
+      }
+      return get_body(clientBuffer, contentLength, true)
+    }
+
+    if line contains ContentLengthHeader then {
+      val extractedContentLength = (line substring ContentLengthHeader.length).toInt
+      return get_body(clientBuffer, extractedContentLength, startBody)
+    }
+    get_body(clientBuffer, contentLength, startBody)
+}
--- middleware/webapp.py
+++ middleware/webapp.py
@@ -1,25 +1,47 @@
-import google.oauth2.credentials
 import google_auth_oauthlib.flow
 
-from flask import Flask, redirect
+from flask import Flask, redirect, request
+
+import json
 
 
 app = Flask(__name__)
 
 
+scopes = [
+    "https://www.googleapis.com/auth/gmail.metadata",
+    "https://www.googleapis.com/auth/gmail.modify",
+]
+
 flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-    '/home/allan/code.allan/g-caos-cleaner/middleware/credentials.json',
-    scopes=['https://www.googleapis.com/auth/gmail.metadata', 'https://mail.google.com/'])
+    "/home/allan/code.allan/g-caos-cleaner/middleware/credentials.json", scopes=scopes
+)
 
-flow.redirect_uri = 'http://localhost:9999'
+flow.redirect_uri = "http://localhost:9999"
 
 
-@app.route("/", methods=['GET'])
+@app.get("/")
 def redirect_to_google():
     authorization_url, state = flow.authorization_url(
-    # Enable offline access so that you can refresh an access token without
-    # re-prompting the user for permission. Recommended for web server apps.
-    access_type='offline',
-    # Enable incremental authorization. Recommended as a best practice.
-    include_granted_scopes='true')
+        # Enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type="offline",
+        # Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes="true",
+    )
     return redirect(authorization_url)
+
+
+@app.post("/access-token")
+def request_access_token():
+    flow.fetch_token(code=request.json["authorization_code"])
+    return (
+        json.dumps(
+            {
+                "access_token": flow.credentials.token,
+                "scopes": flow.credentials.scopes,
+                "refresh_token": flow.credentials.refresh_token,
+            }
+        ),
+        200,
+    )
